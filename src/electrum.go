8207e0c04eb8f8efe8eddb3b337cbd1ab86f6526
Jakub Nawalaniec <pielgrzym@prymityw.pl>
2016-03-14 19:37:11 +0100

    do not use init script

--- src/electrum.go
+++ src/electrum.go
@@ -154,7 +154,7 @@ func electrumStatus(w http.ResponseWriter) (res jsonObject) {
 
 	if strings.Compare(json_response, "Daemon not running\n") == 0 && err != nil {
 		status.Log(syslog.LOG_NOTICE, "Starting Electrum daemon")
-		cmd := exec.Command("sudo", "/etc/init.d/S99electrum", "start")
+		cmd := exec.Command("proxychains4", "electrum", "daemon", "start")
 		err = cmd.Start()
 		status.Log(syslog.LOG_NOTICE, "Done.")
 
